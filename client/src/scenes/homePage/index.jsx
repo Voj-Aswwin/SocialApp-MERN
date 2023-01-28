47493ca0a0415786e1af6ad2cdbc45aa23af09e9
Vojaswwin <vojaswwin.ap@thoughtworks.com>
2023-01-28 18:29:48 +0530

    Add profile page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,10 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navBar";
 import UserWidget from "scenes/widgets/UserWidgets";
+import MyPostWidget from "scenes/widgets/MyPostsWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendsList";
 
 const HomePage = () => {
 
@@ -10,19 +14,33 @@ const HomePage = () => {
     console.info(isNonMobileScreens)
 
     return (
-        <Box>
-            <Navbar />
-            <Box
-                width="100%"
-                padding="2rem 6%"
-                // display={isNonMobileScreens ? "flex" : "block"}
-                gap="0.5rem"
-                justifyContent="space-between"
-            >
-                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-                    <UserWidget userId={_id} picturePath={picturePath} />
-                </Box>
+    <Box>
+        <Navbar />
+        <Box
+          width="100%"
+          padding="2rem 6%"
+          display={isNonMobileScreens ? "flex" : "block"}
+          gap="0.5rem"
+          justifyContent="space-between"
+        >
+          <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+            <UserWidget userId={_id} picturePath={picturePath} />
+          </Box>
+          <Box
+            flexBasis={isNonMobileScreens ? "42%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+          >
+            <MyPostWidget picturePath={picturePath} />
+            <PostsWidget userId={_id} />
+          </Box>
+          {isNonMobileScreens && (
+            <Box flexBasis="26%">
+              <AdvertWidget />
+              <Box m="2rem 0" />
+              <FriendListWidget userId={_id} />
             </Box>
-        </Box>);
+          )}
+        </Box>
+      </Box>);
 }
 export default HomePage
